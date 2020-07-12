2c98b1f423fb1673d48b0190d265c58c018be314
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-07-12 17:20:59 -0400

    References list with links

--- docs/bibliography.jl
+++ docs/bibliography.jl
@@ -1,9 +1,10 @@
 using Documenter
 using Documenter.Expanders
 using Documenter.Expanders: iscode
+using Documenter.Selectors
 using Documenter.Documents
 
-using Bibliography: xnames, xyear, xtitle, xin
+using Bibliography: xnames, xyear, xlink, xtitle, xin
 
 abstract type BibliographyBlock <: Expanders.ExpanderPipeline end
 
@@ -11,10 +12,14 @@ Selectors.order(::Type{BibliographyBlock}) = 12.0
 Selectors.matcher(::Type{BibliographyBlock}, node, page, doc) = iscode(node, r"^@bibliography")
 
 function Selectors.runner(::Type{BibliographyBlock}, x, page, doc)
+    @info "Expanding bibliography."
     raw_bib = "<dl>"
     for entry in BIBLIOGRAPHY
+        @info "Expanding bibliography entry: $(entry.id)."
         entry_text = """<dt>$(entry.id)</dt>
-        <dd>$(xnames(entry)) ($(xyear(entry))), $(xtitle(entry)), $(xin(entry))</dd>
+        <dd>
+          <div id="$(entry.id)">$(xnames(entry)) ($(xyear(entry))), <a href="$(xlink(entry))">$(xtitle(entry))</a>, $(xin(entry))</a>
+        </dd>
         """
         raw_bib *= entry_text
     end
