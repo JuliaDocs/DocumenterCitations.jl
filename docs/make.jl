0c82e84831370bf3c4bbf4f7a22b4f00c7f1d58d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-07-12 15:18:21 -0400

    Page that says hi

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,10 @@ using Bibliography
 
 const BIBLIOGRAPHY = import_bibtex("test.bib")
 
-makedocs(sitename="Testing BibTeX citations and references")
+makedocs(
+    sitename = "Testing BibTeX citations and references",
+      format = Documenter.HTML(
+          prettyurls = get(ENV, "CI", nothing) == "true"
+      )
+)
 
---
+++ docs/src/index.md
@@ -0,0 +1,11 @@
+# DocumenterBibliographyTest.jl documentation
+
+Hi!
+
+In this page I will cite an article [ahu61](@cite) and a book [ab94](@cite).
+
+# References
+
+```@bibliography
+```
+
