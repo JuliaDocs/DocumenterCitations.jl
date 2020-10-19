0aebd78dd16d8b92f6cbf903de28308314e01c12
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-10-19 09:15:18 -0700

    Add plugin type

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,17 @@
-using Documenter
 using DocumenterCitations
+const root_dir = dirname(dirname(pathof(DocumenterCitations)))
+const doc_dir = joinpath(root_dir, "docs")
 import DocumenterCitations
+
+using Documenter
 using Bibliography
 
-DocumenterCitations.BIBLIOGRAPHY() = import_bibtex(joinpath(@__DIR__, "test.bib"))
+cite_bib = CitationBibliography(joinpath(doc_dir, "test.bib"))
 
 makedocs(
+    cite_bib,
     sitename = "Testing BibTeX citations and references",
+    strict = true,
       format = Documenter.HTML(
           prettyurls = get(ENV, "CI", nothing) == "true"
       ),
