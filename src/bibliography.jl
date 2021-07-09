a5513b091b984aea0c4753eb2a984174989da2e6
Johann Antonio Duffek <75576197+LazyScholar@users.noreply.github.com>
2021-07-09 16:38:11 +0200

    more substitutions and final curly brace stripping (#49)

--- src/bibliography.jl
+++ src/bibliography.jl
@@ -6,30 +6,31 @@ Selectors.matcher(::Type{BibliographyBlock}, node, page, doc) = Expanders.iscode
 const tex2unicode_replacements = (
     "---" => "—", # em dash needs to go first
     "--"  => "–",
-    r"\\`\{(\S)\}" => s"\1\u300", # \`{o} 	ò 	grave accent
-    r"\\'\{(\S)\}" => s"\1\u301", # \'{o} 	ó 	acute accent
-    r"\\\^\{(\S)\}" => s"\1\u302", # \^{o} 	ô 	circumflex
-    r"\\~\{(\S)\}" => s"\1\u303", # \~{o} 	õ 	tilde
-    r"\\=\{(\S)\}" => s"\1\u304", # \={o} 	ō 	macron accent (a bar over the letter)
-    r"\\u\{(\S)\}" => s"\1\u306",  # \u{o} 	ŏ 	breve over the letter
-    r"\\\.\{(\S)\}" => s"\1\u307", # \.{o} 	ȯ 	dot over the letter
-    r"\\\\\"\{(\S)\}" => s"\1\u308", # \"{o} 	ö 	umlaut, trema or dieresis
-    r"\\r\{(\S)\}" => s"\1\u30A",  # \r{a} 	å 	ring over the letter (for å there is also the special command \aa)
-    r"\\H\{(\S)\}" => s"\1\u30B",  # \H{o} 	ő 	long Hungarian umlaut (double acute)
-    r"\\v\{(\S)\}" => s"\1\u30C",  # \v{s} 	š 	caron/háček ("v") over the letter
-    r"\\d\{(\S)\}" => s"\1\u323",  # \d{u} 	ụ 	dot under the letter
-    r"\\c\{(\S)\}" => s"\1\u327",  # \c{c} 	ç 	cedilla
-    r"\\k\{(\S)\}" => s"\1\u328",  # \k{a} 	ą 	ogonek
-    r"\\b\{(\S)\}" => s"\1\u331",  # \b{b} 	ḇ 	bar under the letter
+    r"\\`\{(\S{1})\}" => s"\1\u300", # \`{o} 	ò 	grave accent
+    r"\\'\{(\S{1})\}" => s"\1\u301", # \'{o} 	ó 	acute accent
+    r"\\\^\{(\S{1})\}" => s"\1\u302", # \^{o} 	ô 	circumflex
+    r"\\~\{(\S{1})\}" => s"\1\u303", # \~{o} 	õ 	tilde
+    r"\\=\{(\S{1})\}" => s"\1\u304", # \={o} 	ō 	macron accent (a bar over the letter)
+    r"\\u\{(\S{1})\}" => s"\1\u306",  # \u{o} 	ŏ 	breve over the letter
+    r"\\\.\{(\S{1})\}" => s"\1\u307", # \.{o} 	ȯ 	dot over the letter
+    r"\\\\\"\{(\S{1})\}" => s"\1\u308", # \"{o} 	ö 	umlaut, trema or dieresis
+    r"\\r\{(\S{1})\}" => s"\1\u30A",  # \r{a} 	å 	ring over the letter (for å there is also the special command \aa)
+    r"\\H\{(\S{1})\}" => s"\1\u30B",  # \H{o} 	ő 	long Hungarian umlaut (double acute)
+    r"\\v\{(\S{1})\}" => s"\1\u30C",  # \v{s} 	š 	caron/háček ("v") over the letter
+    r"\\d\{(\S{1})\}" => s"\1\u323",  # \d{u} 	ụ 	dot under the letter
+    r"\\c\{(\S{1})\}" => s"\1\u327",  # \c{c} 	ç 	cedilla
+    r"\\k\{(\S{1})\}" => s"\1\u328",  # \k{a} 	ą 	ogonek
+    r"\\b\{(\S{1})\}" => s"\1\u331",  # \b{b} 	ḇ 	bar under the letter
+    r"\\t\{(\S{1})(\S{1})\}" => s"\1\u0361\2",  # \t{oo} 	o͡o 	"tie" (inverted u) over the two letters
     r"\{\}" => s"",  # empty curly braces should not have any effect
     r"\\o" => s"\u00F8",  # \o 	ø 	latin small letter O with stroke
     r"\\O" => s"\u00D8",  # \O 	Ø 	latin capital letter O with stroke
     r"\\l" => s"\u0142",  # \l 	ł 	latin small letter L with stroke
     r"\\L" => s"\u0141",  # \L 	Ł 	latin capital letter L with stroke
     r"\\i" => s"\u0131",  # \i 	ı 	latin small letter dotless I
+    r"\{([[:alnum:]]+)\}" => s"\1",  # {<text>} 	<text> 	bracket stripping after applying all rules
 
     # TODO:
-    # \t{oo} 	o͡o 	"tie" (inverted u) over the two letters
     # \"{\i} 	ï 	Latin Small Letter I with Diaeresis
 
     # Sources : https://www.compart.com/en/unicode/U+0131 enter the unicode character into the search box
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ using Test
 @testset "tex2unicode" begin
     @test DocumenterCitations.tex2unicode("-- ---") == "– —"
     @test DocumenterCitations.tex2unicode(
-      raw"\`{o}\'{o}\^{o}\~{o}\={o}\u{o}\.{o}\\\"{o}\r{a}\H{o}\v{s}\d{u}\c{c}\k{a}\b{b}\~{a}") == "òóôõōŏȯöåőšụçąḇã"
+        raw"\`{o}\'{o}\^{o}\~{o}\={o}\u{o}\.{o}\\\"{o}\r{a}\H{o}\v{s}\d{u}\c{c}\k{a}\b{b}\~{a}") == "òóôõōŏȯöåőšụçąḇã"
     @test DocumenterCitations.tex2unicode(
         raw"\i{}\o{}\O{}\l{}\L{}\i\o\O\l\L") == "ıøØłŁıøØłŁ"
+    @test DocumenterCitations.tex2unicode(
+        raw"\t{oo}{testText}\t{az}") == "o͡otestTexta͡z"
 end
